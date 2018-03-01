9bd61ff67315324ab77d2199fc96659d9e92f755
Juan <kernel.no.found@gmail.com>
2018-03-01 17:45:00 +0000

    Example using node-beacon-scanner

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
-const blue = require('bluetoothctl');
+// const BeaconScanner = require('node-beacon-scanner');
+// const scanner = new BeaconScanner();
+const Gpio = require('onoff').Gpio;
 
-console.log(blue.checkBluetoothController());
+const BUZZER = new Gpio(27, 'out');
+BUZZER.writeSync(1)
+
+while(true) {}
+// // Set an Event handler for becons
+// scanner.onadvertisement = (ad) => {
+//   console.log(JSON.stringify(ad, null, '  '));
+// };
+ 
+// // Start scanning
+// scanner.startScan().then(() => {
+//   console.log('Started to scan.')  ;
+// }).catch((error) => {
+//   console.error(error);
+// });
+
+
