cbbb8a01185e0a1d029e9da948295b53a02c0cce
Juan <kernel.no.found@gmail.com>
2018-03-07 23:33:59 +0000

    Test Deplou

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const BUZZER = new Gpio(27, 'out');
 BUZZER.writeSync(1)
 
 while(true) {}
+
 // // Set an Event handler for becons
 // scanner.onadvertisement = (ad) => {
 //   console.log(JSON.stringify(ad, null, '  '));
