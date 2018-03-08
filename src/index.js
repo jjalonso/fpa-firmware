725604685b8485d1e3a5ca36a4a3b2ea8fd2327c
Juan <kernel.no.found@gmail.com>
2018-03-08 01:42:44 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const Buzzer = require('./buzzer.js');
 
 const buzzer = new Buzzer(27);
 const scanner = new Scanner();
-scanner.onSuccessCb(buzzer.beep);
+scanner.onSuccessCb = buzzer.beep;
 scanner.start();
